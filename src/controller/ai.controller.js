550cd8bbbbae0c564b373482c65a5cc9cbf7a979
shyam <shyam85.6kumar@gmail.com>
2025-08-15 12:58:02 +0530

    initial backend commit

---
+++ src/controller/ai.controller.js
@@ -0,0 +1,18 @@
+const aiService = require("../services/ai.service")
+
+module.exports.geReview = async (req, res) => {
+
+
+    const code = req.body.code;
+
+    if (!code) {
+        return res.status(400).send("prompt is required");
+    }
+
+
+    const response = await aiService(code);
+
+
+
+    res.send(response);
+}
