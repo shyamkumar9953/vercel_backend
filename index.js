bc68be52289c06289be08699d73940b0a86e6263
shyam <shyam85.6kumar@gmail.com>
2025-08-15 15:01:51 +0530

    fixed backend for vercel deployment

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
+ // index.js
 const serverless = require("serverless-http");
 const app = require("./src/app");
 
-if (process.env.VERCEL) {
-  // Running on Vercel (serverless function)
-  module.exports = serverless(app);
-} else {
-  // Running locally
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Server running locally on http://localhost:${PORT}`);
-  });
-}
+module.exports = app; // This allows local dev
+module.exports.handler = serverless(app); // This is for Vercel
